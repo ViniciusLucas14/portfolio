6962483989d66ce6134870f4cdfdfb413d0e0d89
Vinicius Lucas <vlucassouza@gmail.com>
2025-07-29 17:29:23 -0300

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  base: 'portfolio',
+  base: '/portfolio/',
   site: "https://vinicius.vitrinid.com.br",
   integrations: [
     sitemap(),
